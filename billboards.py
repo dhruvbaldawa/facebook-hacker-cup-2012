803dec31465ccffb82df4039dc9dbab85d38800f
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-01-21 12:22:21 +0530

    Solved Billboards, but need to solve again

--- billboards.py
+++ billboards.py
@@ -20,6 +20,15 @@ The text in each case contains at most 1000 characters
 """
 import sys
 import re
+import textwrap
+
+def all_indices(string, sub, listindex=[], offset=0):
+    i = string.find(sub, offset)
+    while i >= 0:
+        listindex.append(i)
+        i = string.find(sub, i + 1)
+    return listindex
+    
 def parse_file():
     lines = open(sys.argv[1]).read()
     for line in lines.splitlines():
@@ -36,7 +45,7 @@ if __name__ == '__main__':
             continue
         
         assert expr.match(line)
-        assert len(line) >= 1 and len(line) <= 1000
+        #assert len(line) >= 1 and len(line) <= 1000
         
         fragments = line.split()
         w,h,s = fragments[0], fragments[1], fragments[2:]
@@ -44,33 +53,59 @@ if __name__ == '__main__':
         # Process
         w = int(w)
         h = int(h)
+        s = " ".join(s)
+        sizes = []
+        wrap_set = []
+        range_ = all_indices(s, " ")
+        for f_width in range_:
+            wrap_set.append(textwrap.wrap(s, f_width, break_long_words=False))
+            # print t
+        for wrap in wrap_set:
+            for f_width in range(1, w):
+                max_w = len(max(wrap, key=len))*f_width
+                max_h = len(wrap)*f_width
+                if max_w <= w and max_h <= h:
+                    sizes.append(f_width)
+        """
         #font_width = w/len(s[0]
         #max_font = font_width
-        sizes = []
-        for f_width in range(1, w):
+        max_line_w = w
+        max_font = 1
+        max_l = []
+        
+        for f_width in range(1, max_line_w):
             l = []
             line_width = w/f_width
             space_left = line_width
-            for word in s:
-                if f_width * len(s) > space_left:
-                    l.append(word)
-                    space_left = line_width - (len(s) * f_width)
-                elif l[-1] not None and l[-1] > space_left:
+            
+            word_pointer = 0
+            line_pointer = 0
+            while True:
+                if word_pointer == len(s) or space_left < 0:
                     break
-                else:
-                    try:
-                        e = l.pop() + " " + word
-                        l.append(e)
-                        space_left = space_left - ((len(s) + 1) * f_width)
-                    except:
-                        l.append(word)
-                        space_left = space_left - (len(s) * f_width)
                 
+                if len(s[word_pointer]) * f_width <= space_left:
+                    space_left -= len(s[word_pointer]) * f_width
+                    try:
+                        e = l.pop()
+                        l.append(e + " " + s[word_pointer])
+                        space_left -= f_width
+                    except:
+                        l.append(s[word_pointer])
+                    word_pointer += 1
+                
+                else:    
+                    space_left = line_width
+                            
             max_w = len(max(l, key=len)) * f_width
             max_h = len(l) * f_width
+            
             if max_w <= w and max_h <= h:
-                sizes.append(f_width)
+                max_font = f_width
+                max_l = l
         
+        #Process end
+        """
         index += 1
         print "Case #%s: %s" % (index, max(sizes))
     pass
